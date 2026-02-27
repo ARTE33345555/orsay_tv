87a49ac5a34616d88fecc872c7eb480274fb4be4
ARTE33345555 <135966791+ARTE33345555@users.noreply.github.com>
2026-02-27 11:27:00 +0200

    Update privilege_manager.h

--- core/privilege_manager.h
+++ core/privilege_manager.h
@@ -1,1 +1,13 @@
+#pragma once
+#include <string>
+#include <map>
 
+class PrivilegeManager {
+public:
+    void registerSetting(const std::string& key, const std::string& value);
+    void updateSetting(const std::string& key, const std::string& value);
+    std::string getSetting(const std::string& key);
+
+private:
+    std::map<std::string, std::string> settings;
+};
